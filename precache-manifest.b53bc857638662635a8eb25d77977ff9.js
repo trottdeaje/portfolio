27f15b555ba317e8f1bbf9bb16a4463a8f3b049f
Aj <deajetrott@gmail.com>
2020-03-23 16:38:52 -0300

    Updates

---
+++ precache-manifest.b53bc857638662635a8eb25d77977ff9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7459f25162a30d825fc7386b162ce513",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "6cb8c4214155b6b7b285",
+    "url": "/portfolio/static/css/main.6e30a72a.chunk.css"
+  },
+  {
+    "revision": "418f2d5e30a49af8f187",
+    "url": "/portfolio/static/js/2.5937081a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.5937081a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cb8c4214155b6b7b285",
+    "url": "/portfolio/static/js/main.6b4a5fe1.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  },
+  {
+    "revision": "431f0af815cac8df9b8c5a446969983b",
+    "url": "/portfolio/static/media/ambersmith-thumb.431f0af8.png"
+  },
+  {
+    "revision": "142b7c744c28b432a12c1013cc2f707c",
+    "url": "/portfolio/static/media/logo.142b7c74.svg"
+  },
+  {
+    "revision": "465499916b438a32dce910998472c284",
+    "url": "/portfolio/static/media/oxapt-thumb.46549991.png"
+  },
+  {
+    "revision": "a0603947ab5fe5f0b78899a7cdf68381",
+    "url": "/portfolio/static/media/vvs.a0603947.png"
+  }
+]);
