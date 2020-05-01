1c800482c005b35d2b11e5a922da705436d0dc65
Aj <32769513+trottdeaje@users.noreply.github.com>
2020-05-01 19:16:04 -0300

    updates

---
+++ precache-manifest.f9856a6ecbe5b7c25e6f83c125e4c470.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87c71f6b078f1e31c7b970743034389b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0489e54728a1744da014",
+    "url": "/static/css/main.2229c138.chunk.css"
+  },
+  {
+    "revision": "418f2d5e30a49af8f187",
+    "url": "/static/js/2.5937081a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5937081a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0489e54728a1744da014",
+    "url": "/static/js/main.68c545d5.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  },
+  {
+    "revision": "9a0bcdad59826ee5397eefac8fb9d686",
+    "url": "/static/media/https___trottdeaje.github.io_static_media_ambersmith-thumb.431f0af8.9a0bcdad.webp"
+  },
+  {
+    "revision": "3dbe4005d340e5037a9414676e8dcb4d",
+    "url": "/static/media/https___trottdeaje.github.io_static_media_oxapt-thumb.46549991.3dbe4005.webp"
+  },
+  {
+    "revision": "6c11ff6ddf87a51b93f595909f2e2cfd",
+    "url": "/static/media/https___trottdeaje.github.io_static_media_vvs.a0603947.6c11ff6d.webp"
+  },
+  {
+    "revision": "142b7c744c28b432a12c1013cc2f707c",
+    "url": "/static/media/logo.142b7c74.svg"
+  }
+]);
